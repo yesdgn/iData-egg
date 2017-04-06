70156ba25db66f11cc5097d3819cb3caefbfe0d3
yesdgn <yesdgn@qq.com>
2017-04-06 21:10:00 +0800

    login

--- app/service/user.js
+++ app/service/user.js
@@ -2,45 +2,65 @@
 
 const co = require('co');
 const moment = require('moment');
+const CryptoJS = require('crypto-js');
 const returnInfo = require('../returnInfo.js');
 module.exports = app => {
-        app.mysql.insert = co.wrap(app.mysql.insert);
-        app.mysql.beginTransactionScope = co.wrap(app.mysql.beginTransactionScope);
-        return class LoginService extends app.Service {
+    app.mysql.insert = co.wrap(app.mysql.insert);
+    app.mysql.delete = co.wrap(app.mysql.delete);
+    app.mysql.query = co.wrap(app.mysql.query);
+    app.mysql.beginTransactionScope = co.wrap(app.mysql.beginTransactionScope);
+    return class LoginService extends app.Service {
         async login(data) {
-             console.log('11');
+            let sqlStr = `select b.userid,b.password from dgn_login_type a inner join dgn_user b on a.userid=b.userid   
+			        where  a.LoginID='{$req.loginid}' and a.logintype={$req.logintype} and b.usertype={$req.usertype} 
+                    and b.isValid=1 and b.IsDelete=0 and b.isAllowLogon=1 
+                    and curdate()>=b.startdate and curdate()<=b.enddate `;
+            sqlStr = this.ctx.helper.queryFormat(sqlStr, data);
+            const result = await app.mysql.query(sqlStr);
+            if (result.length > 0 && result[0].userid && result[0].password && result[0].password == CryptoJS.SHA1(data.password + result[0].userid).toString()) {
+                let accessToken = this.ctx.helper.generateUUID();
+                let expireTime = moment(new Date()).add(5, 'days').format('YYYY-MM-DD HH:mm:ss');
+                let result1 = await app.mysql.delete('dgn_access_token', {userid: result[0].userid ,appid:data.appid});
+                result1 = await app.mysql.insert('dgn_access_token', { UserID: result[0].userid, AppID: data.appid, AccessToken: accessToken, ExpireTime: expireTime, wx_unionid: data.loginid, wx_session_key: data.sessionkey });
+                return { returnCode: 0, result: 'success', resultDescribe: '登录成功', accessToken: accessToken, userID: result.userid };
+            }
+            else {
+                return { returnCode: -1, result: 'fail', resultDescribe: '用户名或密码错误' };
+            }
+
         }
         async reg(data) {
             const result = await app.mysql.beginTransactionScope(async function (conn) {
                 conn.insert = co.wrap(conn.insert);
                 // don't commit or rollback by yourself
-                const userid=this.ctx.helper.getRand(5);
-                const userImages=this.ctx.helper.getRand(5);
-                const curTime=moment(new Date()).format('YYYY-MM-DD HH:mm:ss');
-                const startTime=moment(new Date()).format('YYYY-MM-DD');
-                const endTime=moment(new Date()).add(10,'years').format('YYYY-MM-DD');
-                let result1 = await conn.insert('dgn_person', { UserID:userid,Code:data.nickname,Name:data.nickname,CreateTime:curTime,UserImages:userImages,wx_unionid:data.loginid }); 
-                if (result1.affectedRows<=0)
-                {throw new Error(JSON.stringify(returnInfo.api.e8000)); }
-                result1 = await conn.insert('dgn_user', { UserID:userid,PassWord:data.password,UserType:data.usertype,StartDate:startTime,EndDate:endTime,IsAllowLogon:1,IsValid:1,CreateTime:curTime }); 
-                if (result1.affectedRows<=0)
-                {throw new Error(JSON.stringify(returnInfo.api.e8000));}
-                result1 = await conn.insert('dgn_login_type', { UserID:userid,LoginID:userid,NickName:data.nickname,LoginType:1}); 
-                if (result1.affectedRows<=0)
-                {throw new Error(JSON.stringify(returnInfo.api.e8000));}
-                result1 = await conn.insert('dgn_login_type', { UserID:userid,LoginID:data.loginid,NickName:data.nickname,LoginType:data.logintype}); 
-                if (result1.affectedRows<=0)
-                {throw new Error(JSON.stringify(returnInfo.api.e8000));}
-                result1 = await conn.insert('dgn_role_user', { UserID:userid,RoleID:'147357647778729163',Isvalid:1}); 
-                if (result1.affectedRows<=0)
-                {throw new Error(JSON.stringify(returnInfo.api.e8000));}
-                let expireTime=moment(new Date()).add(5,'days').format('YYYY-MM-DD HH:mm:ss');
-                let accessToken=this.ctx.helper.generateUUID();
-                result1 = await conn.insert('dgn_access_token', { UserID:userid,AppID:data.appid,AccessToken:accessToken,ExpireTime:expireTime,wx_unionid:data.loginid,wx_session_key:data.sessionkey}); 
-                if (result1.affectedRows<=0)
-                {throw new Error(JSON.stringify(returnInfo.api.e8000));}
-                return {returnCode: 0,result:'success',resultDescribe:'注册成功' ,accessToken:accessToken ,userID:userid};
-            }.bind(this), this.ctx); 
+                const userid = this.ctx.helper.getRand(5);
+                const userImages = this.ctx.helper.getRand(5);
+                const curTime = moment(new Date()).format('YYYY-MM-DD HH:mm:ss');
+                const startTime = moment(new Date()).format('YYYY-MM-DD');
+                const endTime = moment(new Date()).add(10, 'years').format('YYYY-MM-DD');
+                const password = CryptoJS.SHA1(data.password + userid).toString();
+                let result1 = await conn.insert('dgn_person', { UserID: userid, Code: data.nickname, Name: data.nickname, CreateTime: curTime, UserImages: userImages, wx_unionid: data.loginid });
+                if (result1.affectedRows <= 0)
+                { throw new Error(JSON.stringify(returnInfo.api.e8000)); }
+                result1 = await conn.insert('dgn_user', { UserID: userid, PassWord: password, UserType: data.usertype, StartDate: startTime, EndDate: endTime, IsAllowLogon: 1, IsValid: 1, CreateTime: curTime });
+                if (result1.affectedRows <= 0)
+                { throw new Error(JSON.stringify(returnInfo.api.e8000)); }
+                result1 = await conn.insert('dgn_login_type', { UserID: userid, LoginID: userid, NickName: data.nickname, LoginType: 1 });
+                if (result1.affectedRows <= 0)
+                { throw new Error(JSON.stringify(returnInfo.api.e8000)); }
+                result1 = await conn.insert('dgn_login_type', { UserID: userid, LoginID: data.loginid, NickName: data.nickname, LoginType: data.logintype });
+                if (result1.affectedRows <= 0)
+                { throw new Error(JSON.stringify(returnInfo.api.e8000)); }
+                result1 = await conn.insert('dgn_role_user', { UserID: userid, RoleID: '147357647778729163', Isvalid: 1 });
+                if (result1.affectedRows <= 0)
+                { throw new Error(JSON.stringify(returnInfo.api.e8000)); }
+                let expireTime = moment(new Date()).add(5, 'days').format('YYYY-MM-DD HH:mm:ss');
+                let accessToken = this.ctx.helper.generateUUID();
+                result1 = await conn.insert('dgn_access_token', { UserID: userid, AppID: data.appid, AccessToken: accessToken, ExpireTime: expireTime, wx_unionid: data.loginid, wx_session_key: data.sessionkey });
+                if (result1.affectedRows <= 0)
+                { throw new Error(JSON.stringify(returnInfo.api.e8000)); }
+                return { returnCode: 0, result: 'success', resultDescribe: '注册成功', accessToken: accessToken, userID: userid };
+            }.bind(this), this.ctx);
             return result;
         }
     }
--- app/service/weixin.js
+++ app/service/weixin.js
@@ -7,42 +7,55 @@ module.exports = app => {
     app.mysql.query = co.wrap(app.mysql.query);
     return class WeiXinService extends app.Service {
         async login(reqParams) {
+            if (!reqParams.logintype || !reqParams.usertype || !reqParams.appid) {
+                throw new Error(JSON.stringify({ "returnCode": -1, "returnDescribe": "登录失败", items: [{ "result": "fail", "resultDescribe": "请检查传入参数" }] }))
+                return;
+            }
             let res = await this.ctx.curl(`https://api.weixin.qq.com/sns/jscode2session?appid=${app.config.iData.wxXCX.Appid}&secret=${app.config.iData.wxXCX.AppSecret}&js_code=${reqParams.code}&grant_type=authorization_code`, {
                 dataType: 'json',
             });
-            if(res.data.openid)
-            {
+            if (res.data.openid) {
                 const results = await app.mysql.query(`select 1 from dgn_login_type a inner join dgn_user b on a.userid=b.userid   
 			        where  a.LoginID='${res.data.openid}'  and b.usertype=${reqParams.usertype} `);
-                if (results.length===0)
-                {   let wxUserInfo=JSON.parse(reqParams.wxUserInfo);
-                    let params={
-                    loginid:res.data.openid,
-                    logintype:reqParams.logintype,
-                    usertype:reqParams.usertype,
-                    nickname:wxUserInfo.nickName,
-                    checkcode:'nocheck',
-                    password:CryptoJS.SHA1('dgn').toString(),  //默认密码dgn
-                    sessionkey:res.data.session_key,
-                    appid:reqParams.appid
-                };    
-                const result=await this.service.user.reg(params);
-                return result;
+                if (results.length === 0) {
+                    let wxUserInfo = JSON.parse(reqParams.wxUserInfo);
+                    let params = {
+                        loginid: res.data.openid,
+                        logintype: reqParams.logintype,
+                        usertype: reqParams.usertype,
+                        nickname: wxUserInfo.nickName,
+                        checkcode: 'nocheck',
+                        password: CryptoJS.SHA1('dgn').toString(),  //默认密码dgn
+                        sessionkey: res.data.session_key,
+                        appid: reqParams.appid
+                    };
+                    const result = await this.service.user.reg(params);
+                    return result;
                 }
-                else
-                {console.log('login');}
+                else {
+                    let params = {
+                        loginid: res.data.openid,
+                        logintype: reqParams.logintype,
+                        usertype: reqParams.usertype,
+                        checkcode: 'nocheck',
+                        password: CryptoJS.SHA1('dgn').toString(),  //默认密码dgn
+                        sessionkey: res.data.session_key,
+                        appid: reqParams.appid
+                    };
+                    const result = await this.service.user.login(params);
+                    return result;
+                }
             }
-            else
-            {
+            else {
                 throw new Error(JSON.stringify({ "returnCode": -1, "returnDescribe": "登录失败", items: [{ "result": "fail", "resultDescribe": res.data.errmsg }] }))
                 return;
             }
-            return res;
+
         }
-        
- 
 
 
- 
+
+
+
     }
 };
